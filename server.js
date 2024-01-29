abf447901b335e1df04379f5015159693e1e8d61
alextasy <alex05tm@gmail.com>
2024-01-29 18:36:24 +0200

    feat(server): add a readiness probe route

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 import { Server } from 'socket.io';
 import { createServer } from 'http';
-import logic from './socket.js';
-
-const server = createServer();
-const io = new Server(server, { cors: { origin: '*', methods: '*' } });
- 
-logic(io);
+import initSocket from './socket.js';
 
 const PORT = process.env.PORT || 4000;
+
+const server = createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(`Server is running on port ${PORT}`);
+  }
+});
+
+const io = new Server(server, { cors: { origin: '*', methods: '*' } });
+initSocket(io);
+
 server.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
