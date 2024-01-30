63ed9219deb3c62013be6fcb9057cbdf07282eba
alextasy <alex05tm@gmail.com>
2024-01-31 00:32:05 +0200

    feat(socket): handle disconnections

--- socket.js
+++ socket.js
@@ -6,8 +6,18 @@ export default io => {
 
     io.on('connection', socket => {
 
+        socket.on('connectUser', userId => {
+            socket.userId = userId;
+            const roomPlayerWasIn = inGameRooms.find(room => room.visitorId === userId || room.creatorId === userId);
+            if (!roomPlayerWasIn) return;
+
+            if (roomPlayerWasIn.timeOut) clearTimeout(roomPlayerWasIn.timeOut);
+            socket.join(roomPlayerWasIn.id);
+            io.to(socket.id).emit('verifyLastMove', roomPlayerWasIn.lastMove || {}); // Opponent could have made a move while we were disconnected
+        });
+
         function filterRooms(roomId = null) {
-            rooms = rooms.filter(room => room.creatorId !== socket.id && room.id !== roomId);
+            rooms = rooms.filter(room => room.creatorId !== socket.userId && room.id !== roomId);
             io.emit('updateRooms', rooms);
         }
 
@@ -15,7 +25,7 @@ export default io => {
 
         socket.on('createRoom', options => {
             const roomId = uuid();
-            rooms.push({ ...options, id: roomId, creatorId: socket.id });
+            rooms.push({ ...options, id: roomId, creatorId: socket.userId });
             socket.join(roomId);
             socket.emit('roomJoined', roomId);
             socket.broadcast.emit('updateRooms', rooms);
@@ -26,7 +36,7 @@ export default io => {
             socket.emit('roomJoined', roomId);
             io.emit('gameStart');
             const room = rooms.find(room => room.id === roomId);
-            room.visitorId = socket.id;
+            room.visitorId = socket.userId;
             inGameRooms.push(room);
             filterRooms(roomId);
         })
@@ -36,16 +46,25 @@ export default io => {
         })
 
         socket.on('move', ({ figIndex, nextSquareIndex, roomId }) => {
+            const room = inGameRooms.find(room => room.id === roomId);
+            room.lastMove = { figIndex, nextSquareIndex };
             socket.broadcast.to(roomId).emit('move', { figIndex, nextSquareIndex });
         })
 
-        socket.on('leftRoom', filterRooms);
+        socket.on('leftRoom', roomId => {
+            filterRooms();
+            socket.broadcast.to(roomId).emit('opponentDisconnected');
+        });
+
         socket.on('disconnect', () => {
             filterRooms();
-            const roomPlayerWasIn = inGameRooms.find(room => room.visitorId === socket.id || room.creatorId === socket.id);
+            const roomPlayerWasIn = inGameRooms.find(room => room.visitorId === socket.userId || room.creatorId === socket.userId);
             if (!roomPlayerWasIn) return;
-            io.to(roomPlayerWasIn.id).emit('opponentDisconnected');
-            inGameRooms = inGameRooms.filter(room => room.id !== roomPlayerWasIn.id);
+            
+            roomPlayerWasIn.timeOut = setTimeout(() => {
+                io.to(roomPlayerWasIn.id).emit('opponentDisconnected');
+                inGameRooms = inGameRooms.filter(room => room.id !== roomPlayerWasIn.id);
+            }, 15 * 1000);
         });
 
         socket.on('gameOver', roomId => {
